37f864522eb5fe2f5b8d894ea197196572f8ae7f
dt1126711290 <dt1126711290@hotmail.com>
2017-04-10 11:57:22 +0800

    GUI

--- src/study/stu.java
+++ src/study/stu.java
@@ -21,13 +21,68 @@ public class stu {
         pan.add(l_name);
         JTextField tf_name=new JTextField(20);
         pan.add(tf_name);
+
+        JLabel l_pwd=new JLabel("密码");
+        pan.add(l_pwd);
+        JPasswordField password = new JPasswordField(20);
+        password.setEchoChar('*');
+        pan.add(password);
+
         JLabel l_sex= new JLabel("性别");
         pan.add(l_sex);
+        JRadioButton male=new JRadioButton("男",true);
+        JRadioButton female=new JRadioButton("女");
+        ButtonGroup group = new ButtonGroup();
+        group.add(male);
+        group.add(female);
+        pan.add(male);
+        pan.add(female);
+
+        JLabel l_ah= new JLabel("爱好");
+        JCheckBox[] hobby={new JCheckBox("音乐"),
+                            new JCheckBox("足球"),
+                            new JCheckBox("绘画")};
+        pan.add(l_ah);
+        pan.add(hobby[0]);
+        pan.add(hobby[1]);
+        pan.add(hobby[2]);
+
+        JLabel l_yx=new JLabel("院系");
+        pan.add(l_yx);
+        String[] departmentNames={"计算机科学与技术","电子信息与技术"};
+        JComboBox department=new JComboBox(departmentNames);
+        department.setEditable(false);
+        pan.add(department);
+
+        JLabel l_xk=new JLabel("选课");
+        String[] coursesNames={"JAVA","Linux","Python"};
+        JList course = new JList(coursesNames);
+        pan.add(l_xk);
+        pan.add(course);
+
+        JLabel l_jf=new JLabel("加分");
+        pan.add(l_jf);
+        JSlider addition = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
+        addition.setMajorTickSpacing(10);
+        addition.setMinorTickSpacing(5);
+        addition.setPaintTicks(true);
+        addition.setPaintLabels(true);
+        addition.setSnapToTicks(true);
+        pan.add(addition);
+
+        JProgressBar progbar=new JProgressBar(JProgressBar.HORIZONTAL,0,100);
+        progbar.setStringPainted(true);
 
         JButton b_sub=new JButton("确认");
         pan.add(b_sub);
         JButton b_save=new JButton("保存");
         pan.add(b_save);
+
+        JLabel l_jg=new JLabel("录入结果");
+        JTextArea result=new JTextArea(10,30);
+        JScrollPane sp=new JScrollPane(result);
+        pan.add(sp);
+        pan.add(l_jg);
 
         con.add(pan);
         f.setVisible(true);
